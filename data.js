050829c6f3ab320ed819af7af3a5f715582af371
round-pixel <starmuratovo@gmail.com>
2017-12-13 07:19:32 +0300

    Receive data few posts

--- data.js
+++ data.js
@@ -43,7 +43,7 @@ module.exports = {
         src: '/dist/images/summer.jpg',
         alt: 'Some text'
       },
-      message: 'Summer days',
+      message: 'Summer days.',
       metaInfo: {
         createdAt: new Date(2017, 0, 1, 2, 3, 4),
         createdBy: 'Mary',
@@ -51,102 +51,65 @@ module.exports = {
       },
       likes: 2
     },
-    {
-      id: 5,
-      image: {
-        src: '/dist/images/river.jpg'
-      },
-      message: 'The long river',
-      metaInfo: {
-        updatedAt: new Date(2017, 0, 1, 2, 3, 4)
-      }
-    },
-    {
-      id: 6,
-      image: {
-        src: '/dist/images/mountains.jpg'
-      },
-      message: 'Great mountains of australia',
-      metaInfo: {
-        createdAt: new Date(2017, 0, 5, 6, 7, 8),
-        createdBy: 'Mike',
-        updatedAt: new Date(2017, 0, 1, 2, 3, 4)
-      },
-      likes: 10
-    },
-    {
-      id: 7,
-      image: {
-        src: '/dist/images/summer.jpg',
-        alt: 'Some text'
-      },
-      message: 'Summer days',
-      metaInfo: {
-        createdAt: new Date(2017, 0, 1, 2, 3, 4),
-        createdBy: 'Mary',
-        updatedAt: new Date(2017, 0, 4, 3, 2, 1)
-      },
-      likes: 2
-    },
-    {
-      id: 8,
-      image: {
-        src: '/dist/images/river.jpg'
-      },
-      message: 'The long river',
-      metaInfo: {
-        updatedAt: new Date(2017, 0, 1, 2, 3, 4)
-      }
-    },
-    {
-      id: 9,
-      image: {
-        src: '/dist/images/mountains.jpg'
-      },
-      message: 'Great mountains of australia',
-      metaInfo: {
-        createdAt: new Date(2017, 0, 5, 6, 7, 8),
-        createdBy: 'Mike',
-        updatedAt: new Date(2017, 0, 1, 2, 3, 4)
-      },
-      likes: 10
-    },
-    {
-      id: 10,
-      image: {
-        src: '/dist/images/summer.jpg',
-        alt: 'Some text'
-      },
-      message: 'Summer days',
-      metaInfo: {
-        createdAt: new Date(2017, 0, 1, 2, 3, 4),
-        createdBy: 'Mary',
-        updatedAt: new Date(2017, 0, 4, 3, 2, 1)
-      },
-      likes: 2
-    },
-    {
-      id: 11,
-      image: {
-        src: '/dist/images/river.jpg'
-      },
-      message: 'The long river',
-      metaInfo: {
-        updatedAt: new Date(2017, 0, 1, 2, 3, 4)
-      }
-    },
-    {
-      id: 12,
-      image: {
-        src: '/dist/images/mountains.jpg'
-      },
-      message: 'Great mountains of australia',
-      metaInfo: {
-        createdAt: new Date(2017, 0, 5, 6, 7, 8),
-        createdBy: 'Mike',
-        updatedAt: new Date(2017, 0, 1, 2, 3, 4)
-      },
-      likes: 10
-    }
+    // {
+    //   id: 5,
+    //   image: {
+    //     src: '/dist/images/river.jpg'
+    //   },
+    //   message: 'The long river.',
+    //   metaInfo: {
+    //     updatedAt: new Date(2017, 0, 1, 2, 3, 4)
+    //   }
+    // },
+    // {
+    //   id: 6,
+    //   image: {
+    //     src: '/dist/images/mountains.jpg'
+    //   },
+    //   message: 'Great mountains of australia.',
+    //   metaInfo: {
+    //     createdAt: new Date(2017, 0, 5, 6, 7, 8),
+    //     createdBy: 'Mike',
+    //     updatedAt: new Date(2017, 0, 1, 2, 3, 4)
+    //   },
+    //   likes: 10
+    // },
+    // {
+    //   id: 7,
+    //   image: {
+    //     src: '/dist/images/summer.jpg',
+    //     alt: 'Some text'
+    //   },
+    //   message: 'Summer days..',
+    //   metaInfo: {
+    //     createdAt: new Date(2017, 0, 1, 2, 3, 4),
+    //     createdBy: 'Mary',
+    //     updatedAt: new Date(2017, 0, 4, 3, 2, 1)
+    //   },
+    //   likes: 2
+    // },
+    // {
+    //   id: 8,
+    //   image: {
+    //     src: '/dist/images/river.jpg'
+    //   },
+    //   message: 'The long river..',
+    //   metaInfo: {
+    //     updatedAt: new Date(2017, 0, 1, 2, 3, 4)
+    //   }
+    // },
+    // {
+    //   id: 9,
+    //   image: {
+    //     src: '/dist/images/mountains.jpg'
+    //   },
+    //   message: 'Great mountains of australia..',
+    //   metaInfo: {
+    //     createdAt: new Date(2017, 0, 5, 6, 7, 8),
+    //     createdBy: 'Mike',
+    //     updatedAt: new Date(2017, 0, 1, 2, 3, 4)
+    //   },
+    //   likes: 10
+    // }
   ]
 };
